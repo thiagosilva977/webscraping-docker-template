1c38f3dcf0aba887bc4e2c9bf05b56d1b4062e47
thiagosilva977 <thiagosilva977@hotmail.com>
2023-04-11 21:29:30 -0300

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapertype_scrapername',
-    version='1.0.1',
+    version='1.0.0',
     packages=find_packages(),
     url='',
     license='',
